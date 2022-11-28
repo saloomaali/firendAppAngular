d667422ad746c54c98f398a785b5f559d226afe7
Salooma <saloomaali2000@gmail.com>
2022-11-28 12:16:05 +0530

    readVAlue added

--- src/app/add-friend/add-friend.component.ts
+++ src/app/add-friend/add-friend.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AddFriendComponent {
 
+  name = ""
+  friendName = ""
+  friendNickName = ""
+  DescribeYourFriend = ""
+
+  readValue = () =>
+  {
+    let data:any = {"name":this.name,"friendName":this.friendName,"friendNickName":this.friendNickName,"DescribeYourFriend":this.DescribeYourFriend}
+    console.log(data)
+  }
+
 }
